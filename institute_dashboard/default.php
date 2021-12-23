60a3ebfc32bde0ee796ebe72b7c71dd74ecd64e1
Dhyey Kathiriya <dhk22500@gmail.com>
2021-12-23 08:20:36 +0530

    testing git

--- institute_dashboard/default.php
+++ institute_dashboard/default.php
@@ -15,22 +15,21 @@ if ($_SESSION['role'] != "Texas") {
 	<?php include_once("../head.php"); ?>
 	<style>
 		.collapse {
-  &:not(.show) {
-    display: none;
-  }
-}
+			&:not(.show) {
+				display: none;
+			}
+		}
+		.collapsing {
+			height: 0;
+			overflow: hidden;
+			@include transition($transition-collapse);
 
-.collapsing {
-  height: 0;
-  overflow: hidden;
-  @include transition($transition-collapse);
-
-  &.collapse-horizontal {
-    width: 0;
-    height: auto;
-    @include transition($transition-collapse-width);
-  }
-}
+			&.collapse-horizontal {
+				width: 0;
+				height: auto;
+				@include transition($transition-collapse-width);
+			}
+		}
 	</style>
 </head>
 
